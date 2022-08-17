257ff28b8d5d77a147245b319fbe63d55f6582e3
FerdianPio <76025616+FerdianPio@users.noreply.github.com>
2022-08-17 13:56:27 +0700

    init grid enemy
    - basic gird spawn

---
+++ Assets/Scripts/Module/GridSystem/GridSystemView.cs
@@ -0,0 +1,46 @@
+using Agate.MVC.Base;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+namespace Sekamar.SpaceShooter.Module.GridSystem
+{
+    public class GridSystemView : BaseView
+    {
+        [SerializeField] private int _width, _height;
+
+        [SerializeField] private GameObject _tilePrefab;
+
+        private Dictionary<Vector2, GameObject> _tiles;
+
+        void Start()
+        {
+            GenerateGrid();
+        }
+
+        void GenerateGrid()
+        {
+            _tiles = new Dictionary<Vector2, GameObject>();
+            for (int x = 0; x < _width; x++)
+            {
+                for (int y = 0; y < _height; y++)
+                {
+                    var spawnedTile = Instantiate(_tilePrefab, new Vector3(x, y, 10), Quaternion.identity);
+                    spawnedTile.name = $"Tile {x} {y}";
+                    
+                    _tiles[new Vector2(x, y)] = spawnedTile;
+                }
+            }
+
+            foreach(GameObject _o in _tiles.Values)
+            {
+                _o.transform.position -= new Vector3(_width / 2, _height / 2);
+                Debug.Log(_o.transform.position);
+            }
+        }
+
+       
+    }
+
+}
